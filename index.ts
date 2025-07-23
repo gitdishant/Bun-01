64150064940473dac34d7f48ac2434a6d19143e7
dishant <randomv637@gmail.com>
2025-07-23 15:03:19 +0530

    adding routes

--- index.ts
+++ index.ts
@@ -1,6 +1,20 @@
-const read = (text : string) => {
-    return text
-}
+import figlet from "figlet"
+const res : string | undefined = "usd";
 
-const bun = read("hello more");
-console.log(bun)
+const server = Bun.serve({
+    port: 3000,
+    fetch(req){
+        const url = new URL(req.url)
+        if(url.pathname === '/'){
+            const body = figlet.textSync("I am learnign!")
+            return new Response(body)
+        }
+        if(url.pathname = '/about'){
+            const kelo = figlet.textSync("we make Bun Here!")
+            return new Response(kelo)
+        }
+        return new Response("404!")
+    }
+})
+
+console.log(`your learning bun on port ${server.port}`)
